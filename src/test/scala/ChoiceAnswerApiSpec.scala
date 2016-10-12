7ce86d18924871e1304f6476ceca967968dfba5e
tekay <tom.kaesler@mni.thm.de>
2016-10-12 17:31:35 +0200

    add more tests to (choice/freetext) answer api spec

--- src/test/scala/ChoiceAnswerApiSpec.scala
+++ src/test/scala/ChoiceAnswerApiSpec.scala
@@ -14,6 +14,8 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.Matchers
 import services.BaseService
 import org.scalatest._
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import akka.http.scaladsl.server.MissingQueryParamRejection
 
 trait ChoiceAnswerApiSpec extends FunSpec with Matchers with ScalaFutures with BaseService with ScalatestRouteTest with Routes with TestData {
@@ -27,6 +29,42 @@ trait ChoiceAnswerApiSpec extends FunSpec with Matchers with ScalaFutures with B
     it("return empty seq when question is a choicequestion") {
       Get("/question/1/choiceAnswer") ~> choiceAnswerApi ~> check {
         responseAs[JsArray] should be(JsArray())
+      }
+    }
+    it("create choice answer correctly") {
+      val questionId = testChoiceAnswers.head.questionId
+      val sessionId = testChoiceAnswers.head.sessionId
+      val answerOptionId = testAnswerOptions.head.id.get
+      val newChoiceAnswer = ChoiceAnswer(None, questionId, sessionId, answerOptionId)
+      val requestEntity = HttpEntity(MediaTypes.`application/json`, newChoiceAnswer.toJson.toString)
+      Post("/question/" + questionId.toString + "/choiceAnswer", requestEntity) ~> choiceAnswerApi ~> check {
+        response.status should be(OK)
+        val newId: String =  Await.result(Unmarshal(response.entity).to[String], 1.second)
+        Get("/question/" + questionId.toString + "/choiceAnswer") ~> choiceAnswerApi ~> check {
+          responseAs[Seq[ChoiceAnswer]] should have length (testChoiceAnswers.length + 1)
+        }
+      }
+    }
+    it("update choice answer") {
+      val choiceAnswer = testChoiceAnswers.drop(1).head
+      val newAnswerOptionId = testAnswerOptions.drop(1).head.id.get
+      val updatedChoiceAnswer = choiceAnswer.copy(answerOptionId = newAnswerOptionId)
+      val requestEntity = HttpEntity(MediaTypes.`application/json`, updatedChoiceAnswer.toJson.toString)
+      Put("/question/" + choiceAnswer.questionId + "/choiceAnswer/" + choiceAnswer.id.get, requestEntity) ~> choiceAnswerApi ~> check {
+        response.status should be(OK)
+        Get("/question/" + choiceAnswer.questionId.toString + "/choiceAnswer/" + choiceAnswer.id.get) ~> choiceAnswerApi ~> check {
+          responseAs[ChoiceAnswer] should be(updatedChoiceAnswer)
+        }
+      }
+    }
+    it("delete choice answer") {
+      val choiceAnswerId = testChoiceAnswers.head.id.get
+      val questionId = testChoiceAnswers.head.questionId
+      Delete("/question/" + questionId + "/choiceAnswer/" + choiceAnswerId) ~> choiceAnswerApi ~> check {
+        response.status should be(OK)
+        Get("/question/" + questionId + "/choiceAnswer/" + choiceAnswerId) ~> choiceAnswerApi ~> check {
+          response.status should be(NotFound)
+        }
       }
     }
   }
--- src/test/scala/FreetextAnswerApiSpec.scala
+++ src/test/scala/FreetextAnswerApiSpec.scala
@@ -14,6 +14,8 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.Matchers
 import services.BaseService
 import org.scalatest._
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import akka.http.scaladsl.server.MissingQueryParamRejection
 
 trait FreetextAnswerApiSpec extends FunSpec with Matchers with ScalaFutures with BaseService with ScalatestRouteTest with Routes with TestData {
@@ -27,6 +29,45 @@ trait FreetextAnswerApiSpec extends FunSpec with Matchers with ScalaFutures with
     it("return empty seq when question is not a choicequestion") {
       Get("/question/5/freetextAnswer") ~> freetextAnswerApi ~> check {
         responseAs[JsArray] should be(JsArray())
+      }
+    }
+    it("create freetext answer correctly") {
+      val questionId = testFreetextAnswers.head.questionId
+      val sessionId = testFreetextAnswers.head.sessionId
+      val subject = "postSubject"
+      val text = "postText"
+      val newFreetextAnswer = FreetextAnswer(None, questionId, sessionId, subject, text)
+      val requestEntity = HttpEntity(MediaTypes.`application/json`, newFreetextAnswer.toJson.toString)
+      Post("/question/" + questionId.toString + "/freetextAnswer", requestEntity) ~> freetextAnswerApi ~> check {
+        println(response)
+        response.status should be(OK)
+        val newId: String =  Await.result(Unmarshal(response.entity).to[String], 1.second)
+        Get("/question/" + questionId.toString + "/freetextAnswer") ~> freetextAnswerApi ~> check {
+          responseAs[Seq[FreetextAnswer]] should have length (testFreetextAnswersForQuestionOne.length + 1)
+        }
+      }
+    }
+    it("update freetext answer") {
+      val freetextAnswer = testFreetextAnswers.drop(1).head
+      val updatedSubject = "updatedSubject"
+      val updatedText = "updatedText"
+      val updatedFreetextAnswer = freetextAnswer.copy(subject = updatedSubject, text = updatedText)
+      val requestEntity = HttpEntity(MediaTypes.`application/json`, updatedFreetextAnswer.toJson.toString)
+      Put("/question/" + freetextAnswer.questionId + "/freetextAnswer/" + freetextAnswer.id.get, requestEntity) ~> freetextAnswerApi ~> check {
+        response.status should be(OK)
+        Get("/question/" + freetextAnswer.questionId.toString + "/freetextAnswer/" + freetextAnswer.id.get) ~> freetextAnswerApi ~> check {
+          responseAs[FreetextAnswer] should be(updatedFreetextAnswer)
+        }
+      }
+    }
+    it("delete freetext answer") {
+      val freetextAnswerId = testFreetextAnswers.head.id.get
+      val questionId = testFreetextAnswers.head.questionId
+      Delete("/question/" + questionId + "/freetextAnswer/" + freetextAnswerId) ~> freetextAnswerApi ~> check {
+        response.status should be(OK)
+        Get("/question/" + questionId + "/freetextAnswer/" + freetextAnswerId) ~> freetextAnswerApi ~> check {
+          response.status should be(NotFound)
+        }
       }
     }
   }
