d7f78da3c0ea09e668af4dff7c01eb13892d189b
tekay <tom.kaesler@mni.thm.de>
2016-10-12 17:32:12 +0200

    add crud methods for both answer types

--- src/main/scala/services/ChoiceAnswerService.scala
+++ src/main/scala/services/ChoiceAnswerService.scala
@@ -1,8 +1,26 @@
 package services
 
-/**
-  * Created by tekay on 12.10.16.
-  */
-class ChoiceAnswerService {
+import models._
+import slick.driver.MySQLDriver.api._
+import scala.concurrent.Future
 
+object ChoiceAnswerService extends BaseService {
+  def getById(choiceAnswerId: ChoiceAnswerId): Future[ChoiceAnswer] = {
+    choiceAnswersTable.filter(_.id === choiceAnswerId).result.head
+  }
+  def getByQuestionId(questionId: QuestionId): Future[Seq[ChoiceAnswer]] = {
+    choiceAnswersTable.filter(_.questionId === questionId).result
+  }
+
+  def create(choiceAnswer: ChoiceAnswer): Future[ChoiceAnswerId] = {
+    choiceAnswersTable returning choiceAnswersTable.map(_.id) += choiceAnswer
+  }
+
+  def update(choiceAnswer: ChoiceAnswer): Future[Int] = {
+    choiceAnswersTable.filter(_.id === choiceAnswer.id.get).map(a => a.answerOptionId).update(choiceAnswer.answerOptionId)
+  }
+
+  def delete(choiceAnswerId: ChoiceAnswerId): Future[Int] = {
+    choiceAnswersTable.filter(_.id === choiceAnswerId).delete
+  }
 }
--- src/main/scala/services/FreetextAnswerService.scala
+++ src/main/scala/services/FreetextAnswerService.scala
@@ -4,11 +4,25 @@ import models._
 import slick.driver.MySQLDriver.api._
 import scala.concurrent.Future
 
-object AnswerService extends BaseService {
-  def createFreetextAnswer(freetextAnswer: FreetextAnswer): Future[FreetextAnswerId] = {
+object FreetextAnswerService extends BaseService {
+  def getById(freetextAnswerId: FreetextAnswerId): Future[FreetextAnswer] = {
+    freetextAnswersTable.filter(_.id === freetextAnswerId).result.head
+  }
+
+  def getByQuestionId(questionId: QuestionId): Future[Seq[FreetextAnswer]] = {
+    freetextAnswersTable.filter(_.questionId === questionId).result
+  }
+
+  def create(freetextAnswer: FreetextAnswer): Future[FreetextAnswerId] = {
+    println(freetextAnswer)
     freetextAnswersTable returning freetextAnswersTable.map(_.id) += freetextAnswer
   }
-  def createChoiceAnswer(choiceAnswer: ChoiceAnswer): Future[ChoiceAnswerId] = {
-    choiceAnswersTable returning choiceAnswersTable.map(_.id) += choiceAnswer
+
+  def update(freetextAnswer: FreetextAnswer): Future[Int] = {
+    freetextAnswersTable.filter(_.id === freetextAnswer.id.get).map(a => (a.subject, a.content)).update(freetextAnswer.subject, freetextAnswer.text)
+  }
+
+  def delete(freetextAnswerId: FreetextAnswerId): Future[Int] = {
+    freetextAnswersTable.filter(_.id === freetextAnswerId).delete
   }
 }
