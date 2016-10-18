493e9c01e8753e7b34873aa1981f01ba9bd48fc7
tekay <tom.kaesler@mni.thm.de>
2016-10-18 15:31:21 +0200

    add comment api to routes

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,13 +1,14 @@
 import akka.http.scaladsl.server.Directives._
 import api._
 
-trait Routes extends ApiErrorHandler with UserApi with SessionApi with QuestionApi with FreetextAnswerApi with ChoiceAnswerApi {
+trait Routes extends ApiErrorHandler with UserApi with SessionApi with QuestionApi with FreetextAnswerApi with ChoiceAnswerApi with CommentApi {
   val routes =
     pathPrefix("api") {
       userApi ~
       sessionApi ~
       questionApi ~
       freetextAnswerApi ~
-      choiceAnswerApi
+      choiceAnswerApi ~
+      commentApi
     } ~ path("")(getFromResource("public/index.html"))
 }
