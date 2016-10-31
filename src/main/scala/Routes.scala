5d26571c001654bbe41a47f741d052cbe716018b
tekay <tom.kaesler@mni.thm.de>
2016-10-31 11:52:50 +0100

    add featuresApi to routes

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,7 +1,7 @@
 import akka.http.scaladsl.server.Directives._
 import api._
 
-trait Routes extends ApiErrorHandler with UserApi with SessionApi with QuestionApi with FreetextAnswerApi with ChoiceAnswerApi with CommentApi {
+trait Routes extends ApiErrorHandler with UserApi with SessionApi with QuestionApi with FreetextAnswerApi with ChoiceAnswerApi with CommentApi with FeaturesApi {
   val routes =
     pathPrefix("api") {
       userApi ~
@@ -9,6 +9,7 @@ trait Routes extends ApiErrorHandler with UserApi with SessionApi with QuestionA
       questionApi ~
       freetextAnswerApi ~
       choiceAnswerApi ~
-      commentApi
+      commentApi ~
+      featuresApi
     } ~ path("")(getFromResource("public/index.html"))
 }
