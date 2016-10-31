56dd3947c3156d8087ac9f28e93c157e60cd5e1c
tekay <tom.kaesler@mni.thm.de>
2016-10-31 11:16:31 +0100

    add features api

---
+++ src/main/scala/api/FeaturesApi.scala
@@ -0,0 +1,47 @@
+package api
+
+import services.FeaturesService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import models._
+import akka.http.scaladsl.server.Directives._
+import spray.json._
+
+trait FeaturesApi {
+  import mappings.FeatureJsonProtocol._
+
+  val featuresApi = pathPrefix("features") {
+    pathEndOrSingleSlash {
+      post {
+        entity(as[Features]) { features =>
+          complete (FeaturesService.create(features).map(_.toJson))
+        }
+      }
+    } ~
+    pathPrefix(IntNumber) { featuresId =>
+      pathEndOrSingleSlash {
+        get {
+          complete (FeaturesService.getById(featuresId).map(_.toJson))
+        } ~
+        put {
+          entity(as[Features]) { features =>
+            complete (FeaturesService.update(features).map(_.toJson))
+          }
+        } ~
+        delete {
+          complete (FeaturesService.delete(featuresId).map(_.toJson))
+        }
+      }
+    } ~
+    pathPrefix("session") {
+      pathPrefix(IntNumber) { sessionId =>
+        path("features") {
+          get {
+            complete (FeaturesService.getBySessionid(sessionId).map(_.toJson))
+          }
+        }
+      }
+    }
+  }
+}
