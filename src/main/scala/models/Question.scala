58cee2269be1f99ac376c22a7043f16ac076d3b4
tekay <tom.kaesler@mni.thm.de>
2016-12-14 13:27:47 +0100

    clean up code fix testdata

--- src/main/scala/models/Question.scala
+++ src/main/scala/models/Question.scala
@@ -1,8 +1,7 @@
 package models
 
-case class Attribute(key: String, value: String)
-
 case class FormatAttributes(attributes: Map[String, String])
+
 case class Question(
                      id: Option[QuestionId],
                      sessionId: SessionId,
@@ -13,11 +12,3 @@ case class Question(
                      formatAttributes: FormatAttributes,
                      answerOptions: Option[Seq[AnswerOption]]
                    )
-
-/*case class Freetext(id: Option[QuestionId], sessionId: SessionId, subject: String, content: String, variant: String,
-                    format: String) extends Question
-case class Flashcard(id: Option[QuestionId], sessionId: SessionId, subject: String, content: String, variant: String,
-                     format: String, backside: String) extends Question
-case class ChoiceQuestion(id: Option[QuestionId], sessionId: SessionId, subject: String, content: String,
-                          variant: String, format: String, answerOptions: Seq[AnswerOption]) extends Question
-*/
