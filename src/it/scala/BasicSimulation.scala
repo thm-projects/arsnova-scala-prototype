425d8b839d703c9ea5da7d28f049ef738f6b6495
Tom Käsler <tom.kaesler@mni.thm.de>
2017-03-18 11:36:55 +0100

    add basic simulation

---
+++ src/it/scala/BasicSimulation.scala
@@ -0,0 +1,26 @@
+import io.gatling.core.Predef._ // 2
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseURL("http://localhost:9000")
+    .inferHtmlResources(BlackList(""".*\.css""", """.*\.js""", """.*\.ico"""), WhiteList())
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .doNotTrackHeader("1")
+    .userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0")
+
+  val headers_0 = Map("Upgrade-Insecure-Requests" -> "1")
+
+  val uri1 = "http://localhost:9000/session/1"
+
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_0")
+      .get("/session/1")
+      .headers(headers_0))
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
